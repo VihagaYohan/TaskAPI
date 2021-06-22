83b8f415e9f0ce2ad76a6fdd28d3aa4ad0556111
VihagaYohan <vihagayohan94@gmail.com>
2021-06-22 19:59:40 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetTask() 
+		{
+			var tasks = new String[] { "Task 1", "Task 2", "Task 3" };
+			return Ok(tasks);
+		}
+
+		[HttpPost]
+		public IActionResult NewTask() 
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		public IActionResult UpdateTask() 
+		{
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteTask() 
+		{
+			return Ok();
+		}
+	}
+}
