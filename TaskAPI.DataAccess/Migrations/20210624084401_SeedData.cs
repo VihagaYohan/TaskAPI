f419ed326b83b3693dc18db49e8b5b6e884cf3e8
VihagaYohan <vihagayohan94@gmail.com>
2021-06-24 14:21:33 +0530

    Implemented database migrations with EntityFramework core

---
+++ TaskAPI.DataAccess/Migrations/20210624084401_SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Created", "Description", "Due", "Status", "Title" },
+                values: new object[] { 1, new DateTime(2021, 6, 24, 14, 14, 0, 852, DateTimeKind.Local).AddTicks(8071), "todo description 1", new DateTime(2021, 6, 29, 14, 14, 0, 853, DateTimeKind.Local).AddTicks(8519), 0, "Todo 1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ TaskAPI.DataAccess/TodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskAPI.DataAccess
+{
+	public class TodoDbContext:DbContext
+	{
+		public DbSet<Todo> Todos { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var connectionString = "Server=LAPTOP-M8FO79SF\\SQLEXPRESS;Database=MyTodoDb;User Id=sa;Password=123456";
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Todo>().HasData(new Todo
+			{
+				Id = 1,
+				Title = "Todo 1",
+				Description = "todo description 1",
+				Created = DateTime.Now,
+				Due = DateTime.Now.AddDays(5),
+				Status=TodoStatus.New
+			}); 
+		}
+	}
+}
